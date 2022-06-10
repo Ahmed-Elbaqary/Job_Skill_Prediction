3527fdb040b835f8baec98d397a1298e3de068d5
Ahmed Elbaqary <ahmedbaqary@gmail.com>
2022-06-11 00:06:09 +0200

    First structure of the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An e2e data science project that uses stackoverflow 2021 survey data to predict the most suitable job for a user with specific skill, and finally simulating the needed skills to start a career in a new wanted job.',
+    author='Ahmed-Elbaqary',
+    license='',
+)
